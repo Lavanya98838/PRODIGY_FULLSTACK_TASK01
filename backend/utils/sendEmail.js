459d5b727825cfa84f42f13852a3c6e322e0f45c
Lavanya98838 <lavanyahardas8@gmail.com>
2026-03-27 18:06:40 +0530

    switch brevo http api for mail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,37 +1,34 @@
-const nodemailer = require("nodemailer");
+const Brevo = require("@getbrevo/brevo");
 
 const sendOTPEmail = async (email, otp) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  const apiInstance = new Brevo.TransactionalEmailsApi();
 
-  const mailOptions = {
-    from: `"MyAuthApp" <${process.env.EMAIL_USER}>`,
-    to: email,
-    subject: "Password Reset OTP",
-    html: `
-      <h2>Password Reset OTP</h2>
-      <p>You requested a password reset. Use the OTP below:</p>
-      <h1 style="
-        background-color: #f0f2f5;
-        padding: 20px;
-        text-align: center;
-        letter-spacing: 10px;
-        font-size: 40px;
-        color: #333;
-      ">${otp}</h1>
-      <p>This OTP expires in <strong>10 minutes</strong>.</p>
-      <p>If you did not request this please ignore this email.</p>
-    `,
+  apiInstance.authentications["apiKey"].apiKey = process.env.BREVO_API_KEY;
+
+  const sendSmtpEmail = new Brevo.SendSmtpEmail();
+
+  sendSmtpEmail.subject = "Password Reset OTP";
+  sendSmtpEmail.to = [{ email: email }];
+  sendSmtpEmail.sender = {
+    name: "MyAuthApp",
+    email: process.env.EMAIL_USER,
   };
+  sendSmtpEmail.htmlContent = `
+    <h2>Password Reset OTP</h2>
+    <p>You requested a password reset. Use the OTP below:</p>
+    <h1 style="
+      background-color: #f0f2f5;
+      padding: 20px;
+      text-align: center;
+      letter-spacing: 10px;
+      font-size: 40px;
+      color: #333;
+    ">${otp}</h1>
+    <p>This OTP expires in <strong>10 minutes</strong>.</p>
+    <p>If you did not request this please ignore this email.</p>
+  `;
 
-  await transporter.sendMail(mailOptions);
+  await apiInstance.sendTransacEmail(sendSmtpEmail);
   console.log(`OTP email sent successfully to ${email}`);
 };
 
