6a92287f8c2e9377416c3a1de29c9ddeb624a33d
Lavanya98838 <lavanyahardas8@gmail.com>
2026-03-31 05:44:25 +0530

    colour change in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
       {/* Greeting Navbar - only shown when logged in */}
       {user && (
         <div style={{
-          backgroundColor: "#875e5e00",
+          backgroundColor: "#00000000",
           padding: "8px 20px",
           display: "flex",
           alignItems: "center",
